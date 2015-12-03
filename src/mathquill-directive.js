5a4bc5907ab822fde9d4b3759652da7debeb2a3b
Jonny Cook <cookj7@users.noreply.github.com>
2015-12-03 15:57:36 +0000

    Update mathquill-directive.js

--- src/mathquill-directive.js
+++ src/mathquill-directive.js
@@ -12,23 +12,39 @@ module.directive('mathquill', ['$interval', '$timeout', function ($interval, $ti
         link: function (scope, element, attrs, ngModel) {
             var mathquill = !!scope.readonly ? element.mathquill() : element.mathquill('editable');
 
-            var latexWatcher = $interval(function() {
+            var latexWatcher = $interval(function () {
                 ngModel.$setViewValue(mathquill.mathquill('latex'));
             }, 500);
 
-            scope.$on('$destroy', function() {
+            scope.$on('$destroy', function () {
                 $interval.cancel(latexWatcher);
             });
 
-            ngModel.$render = function() {
+            ngModel.$render = function () {
                 mathquill.mathquill('latex', ngModel.$viewValue || '');
             };
 
-            $timeout(function() {
-                mathquill.find("textarea").focus();
-
+            $timeout(function () {
+                scope.focus();
             }, 200);
 
+            /**
+             * mathquillCmd broadcast handler
+             */
+            scope.$on('mathquill.command', function (event, command) {
+                if (!!scope.readonly) {
+                    return;
+                }
+                mathquill.mathquill('cmd', command);
+                scope.focus();
+            });
+
+            /**
+             * focus on mathquill element
+             */
+            scope.focus = function () {
+                mathquill.find("textarea").focus();
+            };
         }
     };
 }]);
